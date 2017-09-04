85cdba353cbf2246eafe65891209fc9d956a223c
Tonee Young <itisi@toneeyoung.com>
2017-09-03 21:49:04 -0400

    Add tests for password validation, start on tests for email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 8 }
   has_secure_password
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -15,16 +15,38 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-    # test "name should not be too long" do
-     #  @user.name = "a" * 51
-     # assert_not @user.valid?
-    # end
-
    test "name should not be too long" do
       @user.name = "a" * 51
      assert_not @user.valid?
    end
 
-    # TODO : add tests for name length, email length, valid email addresses, & invalid email addresses
-    
+   test "email should be present" do 
+     @user.email = "    "
+     assert_not @user.valid?
+   end
+
+   test "email should not be too long" do
+     @user.email = "a" * 255 + "@domain.com"
+     assert_not @user.valid?
+   end
+
+   # test "email validation should accept valid addresses" do
+     #valid_addresses = %w[ insert valid addresses here  ]
+     #valid_addresses.each do |address|
+        #@user.email = address
+        #assert @user.valid?
+   #end
+
+   #test "email validation should reject invalid addresses" do
+   #end
+
+   test "password should not be blank" do
+     @user.password = @user.password_confirmation = "    " * 6
+     assert_not @user.valid?
+   end
+
+   test "password should not be too short" do
+     @user.password = @user.password_confirmation = "a" * 7
+     assert_not@user.valid?
+   end
 end
