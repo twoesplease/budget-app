b5175c628633a80c57f3b5725255c1a27471e267
Tonee Young <itisi@toneeyoung.com>
2017-09-03 21:37:54 -0400

    Add secure password requirement to user model & create corresponding test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    uniqueness: { case_sensitive: false }
+  has_secure_password
 end
