1c209881b9e8c11f5e66f88484c72b22c70b6c85
Tonee Young <itisi@toneeyoung.com>
2017-09-04 21:07:42 -0400

    Add email address validation & test for unique email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
+  before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
+  # source: Hartl Rails tutorial + a couple of my own tweaks 
+  VALID_EMAIL_REGEX = /\A[\w][\w\-\.\+]+@([\w\-]+\.)+[\w\-]{2,4}\z/i 
   validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false }
   validates :password, presence: true, length: { minimum: 8 }
   has_secure_password
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,9 +6,9 @@ class UserTest < ActiveSupport::TestCase
                      password: "mY*t3$tP@55w3RD", password_confirmation: "mY*t3$tP@55w3RD")
   end
 
-    test "user should be valid" do
-      assert @user.valid?
-    end
+  test "user should be valid" do
+    assert @user.valid?
+  end
 
   test "name should be present" do
     @user.name = "     "
@@ -30,15 +30,30 @@ class UserTest < ActiveSupport::TestCase
      assert_not @user.valid?
    end
 
-   # test "email validation should accept valid addresses" do
-     #valid_addresses = %w[ insert valid addresses here  ]
-     #valid_addresses.each do |address|
-        #@user.email = address
-        #assert @user.valid?
-   #end
+   test "email validation should accept valid addresses" do
+     valid_addresses = %w[valid@example.com valid_address@example.org val.id@example.com val+id@example.COM 
+                      VALID_again@example.org vAlId_AdDrEsS@example.cc valid-address@EXAMPLE.com]
+     valid_addresses.each do |address|
+        @user.email = address
+        assert @user.valid?, "#{address.inspect} should be valid"
+     end
+   end
 
-   #test "email validation should reject invalid addresses" do
-   #end
+   test "email validation should reject invalid addresses" do
+     invalid_addresses = %w[invalid@ invalid_address@example+another.com -invalid@example.com .invalid@example.org 
+                             invalid@example,org invalid@example i@d.org  invalid@example_com invalid_at_example.com]
+    invalid_addresses.each do |address|
+      @user.email = address
+      assert_not @user.valid?, "#{address.inspect} should be invalid"
+    end
+   end
+
+   test "email addresses should be unique" do
+     duplicate_user = @user.dup
+     duplicate_user.email = @user.email.upcase
+     @user.save
+     assert_not duplicate_user.valid?
+   end
 
    test "password should not be blank" do
      @user.password = @user.password_confirmation = "    " * 6
