bd45186a70725cc417c015470a8b96e49d28e327
Tonee Young <itisi@toneeyoung.com>
2017-09-07 20:09:02 -0400

    Remove 'string' column from Users db

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905011408) do
+ActiveRecord::Schema.define(version: 20170908000145) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "name"
     t.integer "budget"
     t.string "password_digest"
-    t.string "string"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
