d64a492e26241de08a8bdd57a82c293b7d53e0ea
Tonee Young <itisi@toneeyoung.com>
2017-09-13 14:00:39 -0400

    Start on validations and associated tests for Budget model

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,8 @@
 class Budget < ApplicationRecord
+  before_save { self.goal_total.round(2) }
   belongs_to :user
   has_many :transactions
+  validates :name, presence: true,  uniqueness: true
+  # validates :goal_date, presence: true, numericality: true,  
+  # validates :goal_total, presence: true, format: 
 end
