7f94d9c615d68d2e7d58dcf14e0e68125c52c3f2
Tonee Young <itisi@toneeyoung.com>
2017-09-13 13:23:00 -0400

    Run migration for test env, add first budget model tests

--- WHERE_TO_START.rb
+++ WHERE_TO_START.rb
@@ -1,1 +1,1 @@
-puts "* Look up why there is a 'string' field in User model \n*Create Budget model so that users can have budgets \n*Figure out the one-to-many User:Budgets relationship"
+puts "Add validation and tests for new budgets"
--- test/models/budget_test.rb
+++ test/models/budget_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class BudgetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @budget = Budget.new(name: "Test Budget", goal_total: 100.00, goal_date: 1/1/2018)
+  end
+
+  test "should be valid" do
+    assert @budget.valid?
+  end
+
+  test "name should be present" do
+    @budget.name = "   "
+    assert_not @budget.valid?
+  end
 end
