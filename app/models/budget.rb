1ba7d5db0d3b23608a439583656a57b5e095ff92
Tonee Young <itisi@toneeyoung.com>
2017-09-20 22:35:44 -0400

    Try to get budget model's basic validity test to pass

--- app/models/budget.rb
+++ app/models/budget.rb
@@ -1,4 +1,11 @@
 class Budget < ApplicationRecord
-  belongs_to :user
-  has_many :transactions
+   belongs_to :user
+  # has_many :transactions
+
+  validates :name, presence: true, uniqueness: true
+  # validates :goal_date,
+            # presence: true,
+            # date: { after: proc { Date.today }, message:
+                    # 'Must be a valid & future date' }
+  # validates :goal_total, presence: true # , format:
 end
